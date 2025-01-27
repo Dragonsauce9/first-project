af5d13910577a96725256e0a62f06ef9fc5cbb20
Dragonsauce9 <dragonsauce9@gmail.com>
2025-01-27 15:04:40 +0200

    Add dynamic movie rendering at home page

--- src/controllers/home-controller.js
+++ src/controllers/home-controller.js
@@ -1,13 +1,14 @@
-import express, { Router } from 'express';
+import { Router } from 'express';
+import movies from '../movies.js';
 
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', { movies});
     }); 
     
     router.get('/about', (req, res) => {
     res.render('about');    
     });
-    
+
 export default router
