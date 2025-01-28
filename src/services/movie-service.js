19741dde34a80c5554928528c7c238cad0b68cd9
Dragonsauce9 <dragonsauce9@gmail.com>
2025-01-28 20:04:51 +0200

    Add search by name

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -5,8 +5,14 @@ import movies from "../movies.js";
 
 export default {
 
-    getAll() {
-        return movies;
+    getAll(filter = {}) {
+        let result = movies;
+
+        if (filter.search) {
+            result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()))
+        }
+
+        return result;
 
     },
 
