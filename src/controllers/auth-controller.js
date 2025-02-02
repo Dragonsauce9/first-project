a6660e83b8a7bf46cb9ec1ccb963d39728ae11d4
Dragonsauce9 <dragonsauce9@gmail.com>
2025-02-02 18:38:59 +0200

    Add auth service

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -3,6 +3,17 @@ const authController = Router();
 
 authController.get('/register', (req, res) => {
     res.render('auth/register');
+});
+
+authController.post('/register', async (req, res) => {
+    const userData = req.body;
+
+    console.log(userData);
+    
+
+    res.end();
+    
+    
 })
 
 export default authController;
